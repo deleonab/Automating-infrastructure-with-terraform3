57ea64c1c3d58bee1305e1e4c78807881ed8cd3c
Dele Onabowu <delesodade@hotmail.com>
2022-08-03 23:46:57 +0100

    uploaded new files

--- backend.tf
+++ backend.tf
@@ -1,4 +1,5 @@
-# Note: The bucket name may not work for you since buckets are unique globally in AWS, so you must give it a unique name.
+### Note: The bucket name may not work for you since buckets are unique globally in AWS, so you must give it a unique name.
+
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "dele-dev-terraform-bucket"
   # Enable versioning so we can see the full revision history of our state files
@@ -12,5 +13,32 @@ resource "aws_s3_bucket" "terraform_state" {
         sse_algorithm = "AES256"
       }
     }
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+
+### So, let us run terraform apply to provision resources.
+
+### Configure S3 Backend
+
+terraform {
+  backend "s3" {
+    bucket         = "dev-terraform-bucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
